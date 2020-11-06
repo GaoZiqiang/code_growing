c23b4c11f22bcbe52f4d9e62b612632072ab224f
gaoziqiang <gaoziqianghi@163.com>
2020-11-06 09:36:17 +0800

    make some changes

--- 11.05/get_quadratic_equation.py
+++ 11.05/get_quadratic_equation.py
@@ -4,12 +4,27 @@
 
 import cmath
 
+from IPython import embed
+
 def get_quadratic_equation(a,b,c):
     d = (b**2) - (4*a*c)
-    sol1 = (-b + cmath.sqrt(d))/(2*a)
-    sol2 = (-b - cmath.sqrt(d))/(2*a)
+    # 下面进行分类型讨论
 
-    return sol1,sol2
+    if d > 0:
+        print('该方程有两个不同的解：')
+        print((-b + (d**0.5))/(2*a),(-b - (d**0.5))/(2*a))
+    elif d == 0:
+        print('该方程只有一个二重解：')
+        print((-b + (d**0.5))/(2*a))
+    else:
+        print('该方程有两个虚根：')
+        print('第一个解：{0:0.3f}+{1:0.3f}j'.format((-b)/(2*a),((-d)**0.5)/(2*a)))
+        print('第二个解：{0:0.3f}-{1:0.3f}j'.format((-b)/(2*a),((-d)**0.5)/(2*a)))
+        # embed()
+    # sol1 = (-b + cmath.sqrt(d))/(2*a)
+    # sol2 = (-b - cmath.sqrt(d))/(2*a)
+
+    # return sol1,sol2
 
 def main():
     print('构建一元二次方程组')
@@ -19,9 +34,9 @@ def main():
     print('得到的方程组为：{0:0.3f}x^2 + {1:0.3f}x + {2:0.3f} = 0'.format(a,b,c))
 
     print('解方程组得：')
-    sol1,sol2 = get_quadratic_equation(a,b,c)
-    print(sol1)
-    print(sol2)
+    get_quadratic_equation(a,b,c)
+    # print(sol1)
+    # print(sol2)
 
 if __name__ == "__main__":
     main()
