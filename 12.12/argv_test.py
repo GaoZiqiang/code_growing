bf95bc26be79c0f650c57ffda4cc079e236d6218
gaoziqiang <gaoziqianghi@163.com>
2020-12-14 22:21:08 +0800

    python argparse

---
+++ 12.12/argv_test.py
@@ -0,0 +1,9 @@
+# -- coding: utf-8 --
+ 
+from sys import argv
+ 
+(arg0, arg1, arg2, arg3) = argv
+print("The script is called: ", arg0)
+print("Your first variable is: ", arg1)
+print("Your second variable is: ", arg2)
+print("Your third variable is: ", arg3)
